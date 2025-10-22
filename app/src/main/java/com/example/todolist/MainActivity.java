cc655191c955529ca18461c2f09cad775fb7c11b
AlexandrMorozov7 <alex_morozov2025@mail.ru>
2025-10-22 23:32:47 +0700

    Добавил сохранение данных в базу.

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.todolist;
 
 import static android.widget.Toast.LENGTH_SHORT;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -16,7 +19,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+import com.example.todolist.NotesDBHelper;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,7 +27,14 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView RecyclerViewNotes;
     public static final ArrayList<Note> notes = new ArrayList<>();
-    NotesAdapter adapter = new NotesAdapter(notes);
+    ArrayList<Note> notesFromDB = new ArrayList<>();
+    NotesAdapter adapter = new NotesAdapter(notesFromDB);
+
+    private NotesDBHelper dbHelper;
+
+
+
+
 
 
     @Override
@@ -37,6 +47,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //Работа с базой данных.
+        dbHelper = new NotesDBHelper(this);
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+
+
         if (notes.isEmpty()) {
             notes.add(new Note("Backetball", "2,", "3", 2));
             notes.add(new Note("1", "2,", "3", 2));
@@ -45,6 +61,29 @@ public class MainActivity extends AppCompatActivity {
             notes.add(new Note("1", "2,", "3", 1));
             notes.add(new Note("1", "2,", "3", 2));
         }
+        for (Note note : notes){
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(NotesContract.NotesEntry.COLUMN_TITLE, note.getTitle());
+            contentValues.put(NotesContract.NotesEntry.COLUMN_DESCRIPTION, note.getDescription());
+            contentValues.put(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK, note.getDayOfWeek());
+            contentValues.put(NotesContract.NotesEntry.COLUMN_PRIORITY, note.getPriority());
+            database.insert(NotesContract.NotesEntry.TABLE_NAME, null, contentValues);
+        }
+
+        Cursor cursor = database.query(NotesContract.NotesEntry.TABLE_NAME, null,null,null,null,null,null);
+
+
+
+        while(cursor.moveToNext()){
+            String title = cursor.getString(cursor.getColumnIndexOrThrow(NotesContract.NotesEntry.COLUMN_TITLE));
+            String description = cursor.getString(cursor.getColumnIndexOrThrow(NotesContract.NotesEntry.COLUMN_DESCRIPTION));
+            String dayOfWeek = cursor.getString(cursor.getColumnIndexOrThrow(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK));
+            int priority = cursor.getInt(cursor.getColumnIndexOrThrow(NotesContract.NotesEntry.COLUMN_PRIORITY));
+            Note note = new Note(title, description, dayOfWeek, priority);
+            notesFromDB.add(note);
+        }
+        cursor.close();
+
         RecyclerViewNotes = findViewById(R.id.RecyclerViewNotes);
 
         RecyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
@@ -60,6 +99,8 @@ public class MainActivity extends AppCompatActivity {
                 remove(position);
             }
         });
+
+        //Помошник для осущестлвения обработки свайпа в Re.. View
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
             @Override
             public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
@@ -74,6 +115,17 @@ public class MainActivity extends AppCompatActivity {
         itemTouchHelper.attachToRecyclerView(RecyclerViewNotes);
 
     }
+
+
+
+
+
+
+
+
+    //Методы вне onCreate();
+
+
 
     public void remove(int position){
         notes.remove(position);
