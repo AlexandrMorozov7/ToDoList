034a3ab22abc1798d4ccc7a89622d75ee59a2452
AlexandrMorozov7 <alex_morozov2025@mail.ru>
2025-10-22 22:59:30 +0700

    Добавлено и обновлено удаление(свайпом влево и зажатием).

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -8,10 +8,12 @@ import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -22,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView RecyclerViewNotes;
     public static final ArrayList<Note> notes = new ArrayList<>();
-    private NotesAdapter adapter;
+    NotesAdapter adapter = new NotesAdapter(notes);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,23 +46,38 @@ public class MainActivity extends AppCompatActivity {
             notes.add(new Note("1", "2,", "3", 2));
         }
         RecyclerViewNotes = findViewById(R.id.RecyclerViewNotes);
-        NotesAdapter adapter = new NotesAdapter(notes);
+
         RecyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
         RecyclerViewNotes.setAdapter(adapter);
         adapter.setOnNoteClickListener(new NotesAdapter.onNoteClickListener() {
             @Override
             public void onNoteClick(int position) {
-
+            Toast.makeText(getApplicationContext(), "Clicked", LENGTH_SHORT).show();
             }
 
             @Override
             public void onLongClick(int position) {
-                notes.remove(position);
-                adapter.notifyDataSetChanged();
+                remove(position);
             }
         });
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
 
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            remove(viewHolder.getAdapterPosition());
+            }
+        });
+        itemTouchHelper.attachToRecyclerView(RecyclerViewNotes);
 
+    }
+
+    public void remove(int position){
+        notes.remove(position);
+        adapter.notifyDataSetChanged();
     }
     public void onClickAddNote(View view) {
         Intent intent = new Intent(this, AddNewActivity.class);
